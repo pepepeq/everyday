fc29c3ee7ee95c69f84f25b6fb962fae95df9af5
pepe <nlrsrzrcj@gmail.com>
2024-08-09 21:50:16 +0800

    feat:第一天

---
+++ src/ListNode.java
@@ -0,0 +1,16 @@
+/**
+ * name：ListNode
+ * date：2024/8/9
+ * user：nl593
+ * description：
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * name：${NAME}
+ * date：2024/8/9
+ * user：nl593
+ * description：${DESCRIPTION}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
