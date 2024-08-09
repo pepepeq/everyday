e642f8831cbdf6e62121eda774e4b26fe48467f1
pepe <nlrsrzrcj@gmail.com>
2024-08-09 21:50:16 +0800

    feat:第一天

---
+++ src/Solution2.java
@@ -0,0 +1,16 @@
+/**
+ * name：Solution2
+ * date：2024/8/9
+ * user：nl593
+ * description：
+ */
+
+
+
+  public class Solution2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        return null;
+    }
+}
